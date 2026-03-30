070b57877d5fadb361e4ed62a7b8246fc0c1c325
Gabriel Almeida <gabriel-pontes09@hotmail.com>
2026-03-30 18:48:29 -0300

    replace server.js with index.ts entry point

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
 
 const app = next({ dev: false })
 const handle = app.getRequestHandler()
-const port = process.env.PORT || 3000
+const port = parseInt(process.env.PORT || '3000', 10)
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
+    const parsedUrl = parse(req.url!, true)
     handle(req, res, parsedUrl)
   }).listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`)
